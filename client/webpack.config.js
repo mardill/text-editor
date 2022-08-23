157967e419ca19ddc3cdb8b135807e5f9f94acd4
Mary Dillon <mary.dillon09@gmail.com>
2022-08-22 19:00:07 -0700

    fixes database.js and webpack files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,12 +16,58 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+            // generates html file through webpack
+            new HtmlWebpackPlugin({
+              template: './index.html',
+              title: 'Contact Cards'
+            }),
+           
+            // custom service worker
+            new InjectManifest({
+              swSrc: './src-sw.js',
+              swDest: 'src-sw.js',
+            }),
       
+            // manifest.json file creation using logo for photo
+            new WebpackPwaManifest({
+              fingerprints: false,
+              inject: true,
+              name: 'Contact Cards',
+              short_name: 'Contact',
+              description: 'Never forget your contacts!',
+              background_color: '#225ca3',
+              theme_color: '#225ca3',
+              start_url: '/',
+              publicPath: '/',
+              icons: [
+                {
+                  src: path.resolve('src/images/logo.png'),
+                  sizes: [96, 128, 192, 256, 384, 512],
+                  destination: path.join('assets', 'icons'),
+                },
+              ],
+            }),
     ],
 
     module: {
       rules: [
-        
+        // css loader and babel
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
